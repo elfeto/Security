2ec2f84309440d0529d2a39bf199140e9181a10e
elfeto <felipe.torres.cs@gmail.com>
2013-10-13 14:07:26 -0400

    Ya el listado esta hecho, falta new intent

--- src/hello/tab/tabhello/HelloTabActivity.java
+++ src/hello/tab/tabhello/HelloTabActivity.java
@@ -1,12 +1,19 @@
 package hello.tab.tabhello;
 
-import java.util.Vector;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.concurrent.ExecutionException;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.R.string;
 import android.app.TabActivity;
 import android.content.Context;
 import android.content.Intent;
@@ -14,8 +21,14 @@ import android.content.res.Resources;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
-import android.widget.LinearLayout;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TabHost;
 import android.widget.TextView;
 
@@ -40,6 +53,7 @@ public class HelloTabActivity extends TabActivity {
         mTabHst.addTab(mTabHst.newTabSpec("tab_test2").setIndicator("Trolley").setContent(i));
         mTabHst.addTab(mTabHst.newTabSpec("tab_test3").setIndicator("Emergencias").setContent(i));
         mTabHst.setCurrentTab(0);
+
     }
     
     TabHost.OnTabChangeListener handler = new TabHost.OnTabChangeListener() {
@@ -51,55 +65,146 @@ public class HelloTabActivity extends TabActivity {
 			//System.out.print(tabId);
 			if(tabId == "tab_test1")
 			{
-				String yo = null;
-				TextView myText = (TextView)findViewById(R.id.textView1);
-		        myText.setTextColor(Color.BLUE);
-		        myText.setText(yo);
-				Requester requester = new Requester();
-				AsyncTask<String, String, String> result = requester.execute("http://136.145.181.66/~esantos/SecurityService/controllers/GetAllNews.php","");
-				//JSONArray pageName;
-					JSONObject obj;
-					JSONArray arr;
-					try {
-						obj = new JSONObject(result.get());
-						arr = obj.getJSONArray("News");
-						for (int j = 0; j < arr.length(); j++)
+		        try {
+					ListView myList = (ListView)findViewById(R.id.listView1);
+
+			        Requester requester = new Requester();
+					AsyncTask<String, String, String> result = requester.execute("http://136.145.181.66/~esantos/SecurityService/controllers/GetAllNews.php","");
+			        writeToFile(result.get());
+			        String S = readFromFile();
+			        
+						JSONObject obj;
+						JSONArray arr;
+						try {
+							obj = new JSONObject(S);
+							arr = obj.getJSONArray("News");
+							String [] anArray;
+							anArray = new String[arr.length()];
+							for (int j = 0; j < arr.length(); j++)
+							{
+							    String post_id0 = arr.getJSONObject(j).getString("Titulo");
+							    //String post_id1 = arr.getJSONObject(j).getString("Fecha");
+							    //String post_id2 = arr.getJSONObject(j).getString("Hora");
+							    //String post_id3 = arr.getJSONObject(j).getString("Autor");
+							    //String post_id4 = arr.getJSONObject(j).getString("Data");
+							    //String post_id5 = arr.getJSONObject(j).getString("ID");
+							    //String post_id6 = arr.getJSONObject(j).getString("OID");
+							   
+							    anArray[j] = post_id0;
+							}
+							myList = (ListView) findViewById(R.id.listView1);
+							myList.setAdapter(new ArrayAdapter<String>(HelloTabActivity.this,android.R.layout.simple_list_item_1, anArray));
+							myList.setClickable(true);
+							
+							//Helper.getListViewSize(myList);
+
+							
+							
+							myList.setOnItemClickListener(new OnItemClickListener() {
+						        public void onItemClick(AdapterView<?> parent, View view,
+						            int position, long id) {
+						          // When clicked, show a toast with the TextView text
+						if(position == 1)
 						{
-						    String post_id0 = arr.getJSONObject(j).getString("Titulo");
-						    String post_id1 = arr.getJSONObject(j).getString("Fecha");
-						    String post_id2 = arr.getJSONObject(j).getString("Hora");
-						    String post_id3 = arr.getJSONObject(j).getString("Autor");
-						    String post_id4 = arr.getJSONObject(j).getString("Data");
-						    String post_id5 = arr.getJSONObject(j).getString("ID");
-						    String post_id6 = arr.getJSONObject(j).getString("OID");
-						   
-						    //myText.setText(post_id0);
-							post_id0 = post_id0+"\n";
-							myText.append(post_id0);
+						//code specific to first list item    
+						         Intent myIntent = new Intent(view.getContext(), HelloTabActivity.class);
+						             startActivityForResult(myIntent, 0);
 						}
-					} catch (JSONException e) {
-						myText.setText("JSONException");
-						e.printStackTrace();
-					} catch (InterruptedException e) {
-						myText.setText("InterruptedException");
-						e.printStackTrace();
-					} catch (ExecutionException e) {
-						myText.setText("ExecutionException");
-						e.printStackTrace();
-					}	
-			}
+
+						if(position == 2)
+						{
+						//code specific to 2nd list item    
+						         Intent myIntent = new Intent(view.getContext(), HelloTabActivity.class);
+						             startActivityForResult(myIntent, 0);
+						}
+						    }
+						  });
+							
+							
+							
+						} catch (JSONException e) {
+							e.printStackTrace();
+						//} catch (InterruptedException e) {
+						//	e.printStackTrace();
+						//} catch (ExecutionException e) {
+						//	e.printStackTrace();
+						}
+		        } catch (Exception e) {
+		            e.printStackTrace();
+		        }			}
 			else if(tabId == "tab_test2")
 			{
-				TextView myTExt = (TextView)findViewById(R.id.textView1);
-		        myTExt.setTextColor(Color.BLUE);
-				myTExt.setText(tabId);
+				
+				//TextView myTExt = (TextView)findViewById(R.id.textView1);
+		        //myTExt.setTextColor(Color.BLUE);
+				//myTExt.setText(tabId);
+		        
+				
 			}
 			else if(tabId == "tab_test3")
 			{
-				TextView mYText = (TextView)findViewById(R.id.textView1);
-		        mYText.setTextColor(Color.BLUE);
-				mYText.setText(tabId);
+				//TextView mYText = (TextView)findViewById(R.id.textView1);
+		        //mYText.setTextColor(Color.BLUE);
+				//mYText.setText(tabId);
 			}
 		}
 	};
+	/* Checks if external storage is available for read and write */
+	public boolean isExternalStorageWritable() {
+	    String state = Environment.getExternalStorageState();
+	    if (Environment.MEDIA_MOUNTED.equals(state)) {
+	        return true;
+	    }
+	    return false;
+	}
+
+	/* Checks if external storage is available to at least read */
+	public boolean isExternalStorageReadable() {
+	    String state = Environment.getExternalStorageState();
+	    if (Environment.MEDIA_MOUNTED.equals(state) ||
+	        Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+	        return true;
+	    }
+	    return false;
+	}
+	private void writeToFile(String data) {
+	    try {
+	        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(openFileOutput("config.txt", Context.MODE_PRIVATE));
+	        outputStreamWriter.write(data);
+	        outputStreamWriter.close();
+	    }
+	    catch (IOException e) {
+	        Log.e("Exception", "File write failed: " + e.toString());
+	    } 
+	}
+	private String readFromFile() {
+
+	    String ret = "";
+
+	    try {
+	        InputStream inputStream = openFileInput("config.txt");
+
+	        if ( inputStream != null ) {
+	            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+	            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+	            String receiveString = "";
+	            StringBuilder stringBuilder = new StringBuilder();
+
+	            while ( (receiveString = bufferedReader.readLine()) != null ) {
+	                stringBuilder.append(receiveString);
+	            }
+
+	            inputStream.close();
+	            ret = stringBuilder.toString();
+	        }
+	    }
+	    catch (FileNotFoundException e) {
+	        Log.e("login activity", "File not found: " + e.toString());
+	    } catch (IOException e) {
+	        Log.e("login activity", "Can not read file: " + e.toString());
+	    }
+
+	    return ret;
+	}
+	
 }
