b071a90049955396d5bace26a36b3fb595c734dd
elfeto <felipe.torres.cs@gmail.com>
2013-10-29 10:04:02 -0400

    push de xavi

---
+++ src/hello/tab/tabhello/map.java
@@ -0,0 +1,19 @@
+package hello.tab.tabhello;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+ 
+public class map  extends Activity{
+
+    public void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState);
+        setContentView(R.layout.map);
+
+
+    }
+}    
+
+
